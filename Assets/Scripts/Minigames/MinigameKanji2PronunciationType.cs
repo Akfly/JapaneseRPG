8c25a013c8398210f819d3196049e409c331a5fa
Ruben Negredo <ak.fly81@hotmail.com>
2019-09-08 19:07:42 +0200

    First minigame where a mix of kanji, hiraganas and katakanas have to be guessed and written in hiragana or katakana

---
+++ Assets/Scripts/Minigames/MinigameKanji2PronunciationType.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MinigameKanji2PronunciationType : MinigameBase
+{
+    public Text kanjiWord;
+    public InputField userInput;
+    public Button inputButton;
+    public Text correctAnswer;
+    private Word selectedWord;
+
+    public override void StartGame()
+    {
+        base.StartGame();
+        Utils.DebugList(GameManager.instance.wordsToUse);
+        selectedWord = Utils.GetRandomWordByRatio(GameManager.instance.wordsToUse);
+        kanjiWord.text = selectedWord.kanji;
+        inputButton.interactable = true;
+        userInput.interactable = true;
+        userInput.text = "";
+        correctAnswer.text = "";
+    }
+
+    public void CheckCorrect()
+    {
+        inputButton.interactable = false;
+        userInput.interactable = false;
+        string userText = Utils.KataToHira(userInput.text);
+        string toCompareText = Utils.KataToHira(selectedWord.pronunciation);
+        bool isCorrect = userText == toCompareText;
+        correctAnswer.text = toCompareText == selectedWord.pronunciation ? selectedWord.pronunciation :
+            toCompareText + " / " + selectedWord.pronunciation;
+        if (isCorrect)
+        {
+            selectedWord.AddWin();
+        } else
+        {
+            selectedWord.AddFail();
+        }
+#if UNITY_EDITOR
+        Debug.Log(userText + " == " + toCompareText + ": " + isCorrect);
+#endif
+        OnFinish?.Invoke(isCorrect);
+    }
+}
